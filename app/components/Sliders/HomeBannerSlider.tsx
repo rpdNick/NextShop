2e160f259984b3e8fa546e1bc0d52909f7053361
rpdNick <rpd_nick@ukr.net>
2025-12-22 23:30:16 +0200

    test firestore db

--- app/components/Sliders/HomeBannerSlider.tsx
+++ app/components/Sliders/HomeBannerSlider.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { getBanners } from "@/lib/api/getBanners";
+
 export default function HomeBannerSlider() {
+  const [banners, setBanners] = useState<any[]>([]);
+
+  useEffect(() => {
+    getBanners().then(res => {
+      setBanners(res);
+    }).catch(error => {
+      console.error("Error loading banners:", error);
+    });
+  }, []);
+
   return (
   <section className="home_banner_slider mt-8">
-    <div className="container">Banner slider content</div>
+    <div className="container">
+      <h2>Banner slider content</h2>
+      <pre>{JSON.stringify(banners, null, 2)}</pre>
+    </div>
   </section>
   )
 }
