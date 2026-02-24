7c9d67a88dae19cd5eef06d2cc33d8ba836d625e
rpdNick <rpd_nick@ukr.net>
2026-02-24 15:56:36 +0200

    search bar icon

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { Search } from 'lucide-react';
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
@@ -18,7 +19,12 @@ export default function SearchBar() {
       <label htmlFor="searchProducts" className="invisible hidden">
         Search
       </label>
-      <input id="searchProducts" type="search" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search for products" className="border border-gray-300 text-gray-900 rounded-lg focus:shadow-[0_0_0_.25rem_rgba(10,173,10,.25)] focus:ring-green-600 focus:ring-0 focus:border-green-600 block p-2 px-3 disabled:opacity-50 disabled:pointer-events-none w-full text-base focus-visible:outline-none" />
+      <div className="input-group group relative">
+        <input id="searchProducts" type="search" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search for products" className="relative border border-gray-300 text-gray-900 rounded-lg focus:shadow-[0_0_0_.25rem_rgba(10,173,10,.25)] focus:ring-green-600 focus:ring-0 focus:border-green-600 block p-2 px-3 disabled:opacity-50 disabled:pointer-events-none w-full text-base focus-visible:outline-none" />
+        <div className="absolute flex items-center justify-center right-0 top-0 bottom-0 pointer-events-none p-2 px-3 opacity-100 group-focus-within:opacity-0 transition-opacity">
+          <Search className="size-4" />
+        </div>
+      </div>
     </form>
   );
 }
